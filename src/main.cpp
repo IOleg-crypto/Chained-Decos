40f84a2e793f812123ca0650fe2a4648f0a85986
IOleg-sus <guchkooleg7@gmail.com>
2025-07-23 18:24:19 +0300

    current result

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "Window/Window.h"
 
 int main() {
-    std::unique_ptr<Window> pWindow = std::make_unique<Window>(800 ,600 , "Game");
+    auto pWindow = std::make_unique<Window>(800 ,600, "Millennium");
     pWindow->Init();
     pWindow->Run();
 }
