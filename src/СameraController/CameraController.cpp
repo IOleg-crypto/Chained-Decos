c092ec07d8d26fdc45b6b54bffdc39f996488c2b
IOleg-sus <guchkooleg7@gmail.com>
2025-07-30 20:34:41 +0300

    separate camera logic in new class(CameraController)

---
+++ src/СameraController/CameraController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 38063 on 30.07.2025.
+//
+
+#include "CameraController.h"
---
+++ src/СameraController/CameraController.h
@@ -0,0 +1,16 @@
+//
+// Created by 38063 on 30.07.2025.
+//
+
+#ifndef CAMERACONTROLLER_H
+#define CAMERACONTROLLER_H
+
+
+
+class CameraController {
+
+};
+
+
+
+#endif //CAMERACONTROLLER_H
