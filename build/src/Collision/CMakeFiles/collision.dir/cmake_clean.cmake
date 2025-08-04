3ec74b34d3b39dd7307a8334be29e1a712e0c3ce
IOleg-sus <guchkooleg7@gmail.com>
2025-08-04 20:19:00 +0300

    test collision system

---
+++ build/src/Collision/CMakeFiles/collision.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/collision.dir/CollisionSystem.cpp.obj"
+  "CMakeFiles/collision.dir/CollisionSystem.cpp.obj.d"
+  "collision.lib"
+  "collision.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/collision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
