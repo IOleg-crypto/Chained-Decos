30b900b464ada3136e07c8eef73669bcfeee32d7
IOleg-sus <guchkooleg7@gmail.com>
2025-08-06 18:16:03 +0300

    TODO : make map to check full AABB collision detection on

--- src/Collision/CollisionSystem.h
+++ src/Collision/CollisionSystem.h
@@ -9,23 +9,27 @@
 #include <raymath.h>
 #include <vector>
 
-
+//
+// Collision - class that handle all collision system in game engine
+//
 class Collision
 {
-private:
-    Vector3 m_position; // Position of the collision object
-    Vector3 m_size;     // Size of the collision object (e.g., bounding box)
-public:
+  private:
+    Vector3 m_min; // Position of the collision object
+    Vector3 m_max; //
+  public:
     Collision() = default;
     // Constructor to initialize position and size
-    Collision(Vector3 position , Vector3 size);
+    Collision(const Vector3 &center, const Vector3 &size);
     ~Collision() = default;
-    Collision(const Collision &other) = delete;
-    Collision(Collision &&other) = delete;
     // Getters for position and size
     // These methods allow access to the position and size of the collision object
-    Vector3 GetPosition() const;
-    Vector3 GetSize() const;
+    Vector3 GetMin() const;
+    Vector3 GetMax() const;
+    // Methods that checks collision
+    void Update(const Vector3 &center, const Vector3 &size);
+    bool Intersects(const Collision &other) const;
+    bool Contains(const Vector3 &point) const;
 };
 
 #endif // COLLISIONSYSTEM_H
