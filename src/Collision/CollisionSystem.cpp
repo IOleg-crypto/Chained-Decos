d270343dfd7a28f3aaa72e6d02e8a51ad21867f5
IOleg-sus <guchkooleg7@gmail.com>
2025-08-11 13:03:18 +0300

    testing automatic collision system

--- src/Collision/CollisionSystem.cpp
+++ src/Collision/CollisionSystem.cpp
@@ -26,3 +26,49 @@ bool Collision::Contains(const Vector3 &point) const
 }
 Vector3 Collision::GetMin() const { return m_min; }
 Vector3 Collision::GetMax() const { return m_max; }
+
+void Collision::GetMeshModelCollision(Model *model)
+{
+    if (!model || model->meshCount == 0)
+        return;
+
+    Vector3 min = {FLT_MAX, FLT_MAX, FLT_MAX};
+    Vector3 max = {-FLT_MAX, -FLT_MAX, -FLT_MAX};
+
+    // Проходимо по всіх мешах моделі
+    for (int m = 0; m < model->meshCount; m++)
+    {
+        Mesh &mesh = model->meshes[m];
+
+        // Проходимо по всіх вершинах у меші
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            Vector3 v = {mesh.vertices[i * 3 + 0], mesh.vertices[i * 3 + 1],
+                         mesh.vertices[i * 3 + 2]};
+
+            if (v.x < min.x)
+                min.x = v.x;
+            if (v.y < min.y)
+                min.y = v.y;
+            if (v.z < min.z)
+                min.z = v.z;
+
+            if (v.x > max.x)
+                max.x = v.x;
+            if (v.y > max.y)
+                max.y = v.y;
+            if (v.z > max.z)
+                max.z = v.z;
+        }
+    }
+
+    // Обчислюємо центр і розміри bounding box
+    Vector3 size = Vector3Subtract(max, min);
+    Vector3 center = Vector3Add(min, Vector3Scale(size, 0.5f));
+
+    // Оновлюємо внутрішні межі колізії
+    Update(center, size);
+
+    // Малюємо bounding box
+    DrawCubeWires(center, size.x, size.y, size.z, GREEN);
+}
--- src/Engine/Engine.cpp
+++ src/Engine/Engine.cpp
@@ -35,12 +35,12 @@ void Engine::Init()
     SetTargetFPS(144);
     rlImGuiSetup(true); // init ImGui
     InitImGuiFont();
+    m_models.LoadModelsFromJson(PROJECT_ROOT_DIR "/src/models.json");
     InitCollisions();
 }
 
 void Engine::Run()
 {
-    m_models.LoadModelsFromJson(PROJECT_ROOT_DIR "/src/models.json");
 
     while (!WindowShouldClose())
     {
@@ -103,13 +103,12 @@ void Engine::Render()
         EndMode3D();
     }
 
-    EndDrawing();
-
     if (m_showDebug)
     {
         DrawDebugInfo(m_player.GetCameraController()->GetCamera(),
                       m_player.GetCameraController()->GetCameraMode());
     }
+    EndDrawing();
 
     // handle menu actions after frame
     switch (m_menu.GetAction())
@@ -140,21 +139,13 @@ void Engine::LoadPlayerModel()
     Vector3 adjustedPos = m_player.GetPlayerPosition();
     adjustedPos.y -= 1.2f;
 
-    DrawModel(m_playerModel, adjustedPos, 0.8f, WHITE);
+    DrawModel(m_playerModel, adjustedPos, 1.0f, WHITE);
     DrawBoundingBox(m_player.GetPlayerBoundingBox(), GREEN);
 }
 
 void Engine::DrawScene3D()
 {
     DrawPlane((Vector3){0.0f, 0.0f, 0.0f}, (Vector2){800.0f, 800.0f}, LIGHTGRAY);
-
-    DrawCube((Vector3){0, 0, 60}, 40, 10, 40, RED);
-    DrawCube((Vector3){0, 0, 100}, 10, 2, 10, GREEN);
-    DrawCube((Vector3){30, 0, 30}, 10, 5, 10, DARKGRAY);
-    DrawCube((Vector3){-50, 0, 20}, 20, 4, 20, BROWN);
-    DrawCube((Vector3){20, 5, 60}, 10, 2, 10, SKYBLUE);
-    DrawCube((Vector3){-30, 8, 70}, 6, 2, 6, PURPLE);
-
     m_models.DrawAllModels();
 }
 
@@ -196,42 +187,15 @@ void Engine::DrawDebugInfo(const Camera &camera, const int &cameraMode)
 
 void Engine::InitCollisions()
 {
-    m_collisionManager.ClearColliders();
+    Model &arenaModel = m_models.GetModelByName("arc");
+    Collision m_arenaCollision;
+    m_arenaCollision.GetMeshModelCollision(&arenaModel);
+    m_collisionManager.AddCollider(m_arenaCollision);
 
-    Vector3 pos1 = {0, 0, 60.f};
-    Vector3 size1 = {40.f, 10.f, 40.f};
-    Collision collider1(pos1, size1);
-    m_collisionManager.AddCollider(collider1);
-
-    Vector3 pos2 = {0, 0, 100.f};
-    Vector3 size2 = {10.f, 2.f, 10.f};
-    Collision collider2(pos2, size2);
-    m_collisionManager.AddCollider(collider2);
-
-    Vector3 pos3 = {30, 0, 30};
-    Vector3 size3 = {10.f, 5.f, 10.f};
-    Collision collider3(pos3, size3);
-    m_collisionManager.AddCollider(collider3);
-
-    Vector3 pos4 = {-50, 0, 20};
-    Vector3 size4 = {20.f, 4.f, 20.f};
-    Collision collider4(pos4, size4);
-    m_collisionManager.AddCollider(collider4);
-
-    Vector3 pos5 = {20, 5, 60};
-    Vector3 size5 = {10.f, 2.f, 10.f};
-    Collision collider5(pos5, size5);
-    m_collisionManager.AddCollider(collider5);
-
-    Vector3 pos6 = {-30, 8, 70};
-    Vector3 size6 = {6.f, 2.f, 6.f};
-    Collision collider6(pos6, size6);
-    m_collisionManager.AddCollider(collider6);
-
-    Vector3 groundPos = {0, -0.5f, 0};
-    Vector3 groundSize = {800.f, 1.0f, 800.f};
-    Collision groundCollider(groundPos, groundSize);
-    m_collisionManager.AddCollider(groundCollider);
+    // Vector3 groundPos = {0, -0.5f, 0};
+    // Vector3 groundSize = {800.f, 2.0f, 800.f};
+    // Collision groundCollider(groundPos, groundSize);
+    // m_collisionManager.AddCollider(groundCollider);
 }
 
 void Engine::InitInput()
@@ -246,11 +210,16 @@ void Engine::InitInput()
                                  cameraMode = CAMERA_FREE;
                                  camera.up = {0, 1, 0};
                              });
-    m_manager.RegisterAction(KEY_F5, [this]() { m_showDebug = !m_showDebug; });
+    m_manager.RegisterAction(KEY_TWO, [this]() { m_showDebug = !m_showDebug; });
 
     m_manager.RegisterAction(KEY_C, [this]() { m_showCollisionDebug = !m_showCollisionDebug; });
 
-    m_manager.RegisterAction(KEY_ONE, [this] { m_showMenu = true; });
+    m_manager.RegisterAction(KEY_ONE,
+                             [this]
+                             {
+                                 m_showMenu = true;
+                                 EnableCursor();
+                             });
 }
 
 void Engine::HandleKeyboardShortcuts() const { m_manager.ProcessInput(); }
