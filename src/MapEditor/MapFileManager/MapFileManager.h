84c29b6eadb8a0aff0604b9207d203919252547e
IOleg-sus <guchkooleg7@gmail.com>
2025-08-12 16:24:55 +0300

    mapeditor : added model support(testing)

--- src/MapEditor/MapFileManager/MapFileManager.h
+++ src/MapEditor/MapFileManager/MapFileManager.h
@@ -5,27 +5,32 @@
 #ifndef MAPFILEMANAGER_H
 #define MAPFILEMANAGER_H
 
+#include <Model/Model.h>
+#include <raylib.h>
 #include <string>
 #include <vector>
-#include <raylib.h>
-#include <Model/Model.h>
+
 
 // Simple object structure for saving/loading
-struct SerializableObject {
+struct SerializableObject
+{
     Vector3 position;
     Vector3 scale;
     Vector3 rotation;
     Color color;
     std::string name;
     int type;
+    std::string modelName; // For model objects (type=5)
 };
 
 // Simple file manager for map operations
-class MapFileManager {
+class MapFileManager
+{
 public:
     // Basic file operations
-    static bool SaveMap(const std::vector<SerializableObject>& objects, const std::string& filename);
-    static bool LoadMap(std::vector<SerializableObject>& objects, const std::string& filename);
+    static bool SaveMap(const std::vector<SerializableObject> &objects,
+                        const std::string &filename);
+    static bool LoadMap(std::vector<SerializableObject> &objects, const std::string &filename);
 };
 
-#endif // MAPFILEMANAGER_H 
+#endif // MAPFILEMANAGER_H
