9c18161409eb760e9808ce502b6ce64a8d5da6b5
I#Oleg <124497826+IOleg-crypto@users.noreply.github.com>
2025-08-21 15:18:30 +0300

    fixed player movement

--- src/Player/PlayerInput.cpp
+++ src/Player/PlayerInput.cpp
@@ -5,60 +5,34 @@ PlayerInput::PlayerInput(Player *player) : m_player(player) {}
 
 void PlayerInput::ProcessInput()
 {
-    float deltaTime = GetFrameTime();
-
     Vector3 inputDirection = GetInputDirection();
+    if (Vector3Length(inputDirection) < 0.001f)
+        return;
 
+    inputDirection = Vector3Normalize(inputDirection);
     auto [forward, right] = GetCameraVectors();
+
     forward.y = 0.0f;
     right.y = 0.0f;
     forward = Vector3Normalize(forward);
     right = Vector3Normalize(right);
 
-    Vector3 worldMoveDir =
-        Vector3Add(Vector3Scale(right, inputDirection.x), Vector3Scale(forward, inputDirection.z));
-    worldMoveDir.y = 0.0f;
+    Vector3 moveDir = {};
+    moveDir.x = forward.x * inputDirection.z + right.x * inputDirection.x;
+    moveDir.z = forward.z * inputDirection.z + right.z * inputDirection.x;
 
-    if (Vector3Length(worldMoveDir) > 0.001f)
-        worldMoveDir = Vector3Normalize(worldMoveDir);
+    if (Vector3Length(moveDir) > 0.001f)
+        moveDir = Vector3Normalize(moveDir);
 
-    Vector3 velocity = m_player->GetPhysics().GetVelocity();
+    float deltaTime = GetFrameTime();
+    float speed = m_player->GetSpeed();
+    Vector3 velocity = Vector3Scale(moveDir, speed);
 
-    if (Vector3Length(worldMoveDir) > 0.001f)
-    {
-        if (m_player->GetPhysics().IsGrounded())
-        {
-            velocity.x = worldMoveDir.x * m_player->GetSpeed();
-            velocity.z = worldMoveDir.z * m_player->GetSpeed();
-        }
-        else
-        {
-
-            const float AIR_CONTROL = 0.1f;
-            velocity.x += worldMoveDir.x * m_player->GetSpeed() * AIR_CONTROL * deltaTime;
-            velocity.z += worldMoveDir.z * m_player->GetSpeed() * AIR_CONTROL * deltaTime;
-        }
-    }
-    else if (m_player->GetPhysics().IsGrounded())
-    {
-
-        const float FRICTION = 0.85f;
-        velocity.x *= FRICTION;
-        velocity.z *= FRICTION;
-
-        const float STOP_THRESHOLD = 0.1f;
-        if (fabsf(velocity.x) < STOP_THRESHOLD)
-            velocity.x = 0.0f;
-        if (fabsf(velocity.z) < STOP_THRESHOLD)
-            velocity.z = 0.0f;
-    }
-
-    m_player->GetPhysics().SetVelocity(velocity);
+    m_player->Move(Vector3Scale(velocity, deltaTime));
 }
 
 void PlayerInput::HandleJumpInput()
 {
-
     if (IsKeyDown(KEY_SPACE) && m_player->GetPhysics().IsGrounded())
     {
         float jumpImpulse = m_player->GetPhysics().GetJumpStrength() * 3.0f;
@@ -81,9 +55,9 @@ Vector3 PlayerInput::GetInputDirection()
     Vector3 inputDir = {};
 
     if (IsKeyDown(KEY_W))
-        inputDir.z -= 1.0f;
-    if (IsKeyDown(KEY_S))
         inputDir.z += 1.0f;
+    if (IsKeyDown(KEY_S))
+        inputDir.z -= 1.0f;
     if (IsKeyDown(KEY_A))
         inputDir.x -= 1.0f;
     if (IsKeyDown(KEY_D))
@@ -100,11 +74,10 @@ std::pair<Vector3, Vector3> PlayerInput::GetCameraVectors() const
 {
     const Camera &camera = m_player->GetCameraController()->GetCamera();
 
-    Vector3 forward = Vector3Subtract(camera.position, camera.target);
+    Vector3 forward = Vector3Subtract(camera.target, camera.position);
     forward.y = 0.0f;
     forward = Vector3Normalize(forward);
 
-    // Right: перпендикулярно forward та up
     Vector3 right = Vector3Normalize(Vector3CrossProduct(forward, {0, 1, 0}));
 
     return {forward, right};
