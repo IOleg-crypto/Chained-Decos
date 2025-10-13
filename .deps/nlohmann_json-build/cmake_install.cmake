7f603ddf59eceebb5712b1beee49944f94520481
IOleg <124497826+IOleg-crypto@users.noreply.github.com>
2025-10-13 18:00:08 +0300

    fix workflow : update tests and delete parkour settings
    fix vector out of range

--- .deps/nlohmann_json-build/cmake_install.cmake
+++ .deps/nlohmann_json-build/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "D:/gitnext/Chained Decos/build/install")
+  set(CMAKE_INSTALL_PREFIX "D:/gitnext/Chained Decos/cmake-build-debug/install")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
