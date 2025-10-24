7af7eeef53792d73abd399a39b1e1035dadfe140
IOleg <124497826+IOleg-crypto@users.noreply.github.com>
2025-10-24 05:07:03 -0400

    update project and fix build for Linux

--- .deps/raylib-build/raylib/CTestTestfile.cmake
+++ .deps/raylib-build/raylib/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: D:/gitnext/Chained Decos/.deps/raylib-src/src
-# Build directory: D:/gitnext/Chained Decos/.deps/raylib-build/raylib
+# Source directory: /home/kali/Chained-Decos/.deps/raylib-src/src
+# Build directory: /home/kali/Chained-Decos/.deps/raylib-build/raylib
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
