458043f8280a1aaeeef58010077247eb489035c7
I#Oleg <guchkooleg7@gmail.com>
2025-10-24 05:07:03 -0400

    update project and fix build for Linux

--- .deps/raylib-build/raylib/external/glfw/cmake_install.cmake
+++ .deps/raylib-build/raylib/external/glfw/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/gitnext/Chained Decos/.deps/raylib-src/src/external/glfw
+# Install script for directory: /home/kali/Chained-Decos/.deps/raylib-src/src/external/glfw
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "D:/gitnext/Chained Decos/build/install")
+  set(CMAKE_INSTALL_PREFIX "/home/kali/Chained-Decos/build/install")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,6 +27,11 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -34,17 +39,17 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/LLVM/bin/llvm-objdump.exe")
+  set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/gitnext/Chained Decos/.deps/raylib-build/raylib/external/glfw/src/cmake_install.cmake")
+  include("/home/kali/Chained-Decos/.deps/raylib-build/raylib/external/glfw/src/cmake_install.cmake")
 endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/gitnext/Chained Decos/.deps/raylib-build/raylib/external/glfw/install_local_manifest.txt"
+  file(WRITE "/home/kali/Chained-Decos/.deps/raylib-build/raylib/external/glfw/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
