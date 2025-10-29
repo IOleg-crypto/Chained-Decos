fc2bc7a02b06ba7cd3b64e128737b403063cea18
IOleg <guchkooleg7@gmail.com>
2025-10-29 16:56:27 +0200

    repo :delete garbage from repository

--- src/MapEditor/Editor/CameraManager/CameraManager.cpp
+++ src/MapEditor/Editor/CameraManager/CameraManager.cpp
@@ -1,5 +1,6 @@
 #include "CameraManager.h"
 #include "Engine/CameraController/CameraController.h"
+#include <imgui.h>
 
 CameraManager::CameraManager(std::shared_ptr<CameraController> cameraController)
     : m_cameraController(std::move(cameraController))
@@ -10,6 +11,13 @@ void CameraManager::Update()
 {
     if (m_cameraController)
     {
+        // If ImGui is capturing mouse (e.g., hovering tools window), skip camera update
+        const ImGuiIO &io = ImGui::GetIO();
+        if (io.WantCaptureMouse)
+        {
+            return;
+        }
+
         m_cameraController->Update();
     }
 }
