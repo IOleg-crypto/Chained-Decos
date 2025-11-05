c806dbb4b0072a5f42ea126394b80340b949476e
IOleg <guchkooleg7@gmail.com>
2025-11-05 13:29:36 +0200

    Windows : disable glfwSetInputMode(crash), only for Linux

--- .deps/googletest-build/googletest/CTestTestfile.cmake
+++ .deps/googletest-build/googletest/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/kali/Chained-Decos/.deps/googletest-src/googletest
-# Build directory: /home/kali/Chained-Decos/.deps/googletest-build/googletest
+# Source directory: D:/gitnext/Chained Decos/.deps/googletest-src/googletest
+# Build directory: D:/gitnext/Chained Decos/.deps/googletest-build/googletest
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
