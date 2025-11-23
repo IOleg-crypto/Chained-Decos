a1d400f64708c1fe69ec9077b64bfa10690a0446
IOleg <guchkooleg7@gmail.com>
2025-11-23 13:29:44 +0200

    feat: fix remaining paths after src/ removal (pass 8)
    - Pass 8: Fixed Engine.h, ColorParser, Render/Interfaces paths (7 files)
    - Created scene/resources/color/ for ColorParser
    - Total updated: 110+ files across 8 passes
    
    Remaining issues:
    - PhysicsComponent.h location
    - MapFileManager.h location
    - imgui.h include path

---
+++ scene/resources/color/ColorParser.cpp
@@ -0,0 +1,30 @@
+#include "ColorParser.h"
+#include <algorithm>
+#include <cctype>
+
+static const std::unordered_map<std::string, Color> colorMap = {
+    {"white", WHITE},         {"black", BLACK},     {"red", RED},       {"green", GREEN},
+    {"blue", BLUE},           {"yellow", YELLOW},   {"orange", ORANGE}, {"pink", PINK},
+    {"purple", PURPLE},       {"brown", BROWN},     {"gray", GRAY},     {"darkgray", DARKGRAY},
+    {"lightgray", LIGHTGRAY}, {"skyblue", SKYBLUE}, {"lime", LIME},     {"gold", GOLD},
+    {"maroon", MAROON},       {"magenta", MAGENTA}, {"beige", BEIGE},   {"violet", VIOLET}};
+
+Color ParseColorByName(const std::string &colorName)
+{
+    std::string lowerName = colorName;
+    std::transform(lowerName.begin(), lowerName.end(), lowerName.begin(),
+                   [](unsigned char c) { return std::tolower(c); });
+
+    auto it = colorMap.find(lowerName);
+    if (it != colorMap.end())
+    {
+        return it->second;
+    }
+
+    return WHITE; // Default color
+}
+
+const std::unordered_map<std::string, Color> &GetColorMap()
+{
+    return colorMap;
+}
