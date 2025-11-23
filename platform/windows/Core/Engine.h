173b4f8ceaee9a573c2f5f56d68a3cc7a4e61dc3
IOleg <guchkooleg7@gmail.com>
2025-11-23 14:05:00 +0200

    fix: restore Engine.h structure
    - Fixed Engine.h with complete class definition
    - Added IModule.h include
    
    Remaining: 5 errors to investigate

--- platform/windows/Core/Engine.h
+++ platform/windows/Core/Engine.h
@@ -1,46 +1,58 @@
+#pragma once
+
+#include "core/object/kernel/Core/Kernel.h"
+#include "core/object/module/Core/ModuleManager.h"
+#include "core/object/module/Interfaces/IModule.h"
+#include "servers/input/Core/InputManager.h"
+#include "servers/rendering/Core/RenderManager.h"
+#include <memory>
+
+
+// Main Engine class
+class Engine
+{
 public:
-Engine(Kernel *kernel);
-~Engine();
+    Engine(Kernel *kernel);
+    ~Engine();
 
-bool Initialize();
-void Shutdown();
+    bool Initialize();
+    void Shutdown();
 
-// Accessors
-Kernel *GetKernel() const
-{
-    return m_kernel;
-}
-ModuleManager *GetModuleManager() const
-{
-    return m_moduleManager.get();
-}
-RenderManager *GetRenderManager() const
-{
-    return m_renderManager.get();
-}
-InputManager *GetInputManager() const
-{
-    return m_inputManager.get();
-}
+    // Accessors
+    Kernel *GetKernel() const
+    {
+        return m_kernel;
+    }
+    ModuleManager *GetModuleManager() const
+    {
+        return m_moduleManager.get();
+    }
+    RenderManager *GetRenderManager() const
+    {
+        return m_renderManager.get();
+    }
+    InputManager *GetInputManager() const
+    {
+        return m_inputManager.get();
+    }
 
-// Module registration
-void RegisterModule(std::unique_ptr<IModule> module);
+    // Module registration
+    void RegisterModule(std::unique_ptr<IModule> module);
 
-// Debug
-bool IsDebugInfoVisible() const
-{
-    return m_debugInfoVisible;
-}
-void SetDebugInfoVisible(bool visible)
-{
-    m_debugInfoVisible = visible;
-}
+    // Debug
+    bool IsDebugInfoVisible() const
+    {
+        return m_debugInfoVisible;
+    }
+    void SetDebugInfoVisible(bool visible)
+    {
+        m_debugInfoVisible = visible;
+    }
 
 private:
-Kernel *m_kernel;
-std::unique_ptr<ModuleManager> m_moduleManager;
-std::unique_ptr<RenderManager> m_renderManager;
-std::unique_ptr<InputManager> m_inputManager;
-bool m_debugInfoVisible = false;
-}
-;
+    Kernel *m_kernel;
+    std::unique_ptr<ModuleManager> m_moduleManager;
+    std::unique_ptr<RenderManager> m_renderManager;
+    std::unique_ptr<InputManager> m_inputManager;
+    bool m_debugInfoVisible = false;
+};
