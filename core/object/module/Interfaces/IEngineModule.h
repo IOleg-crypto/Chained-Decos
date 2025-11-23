28cfa58275ede64bbee0be52d19ce70a1e3397bd
IOleg <guchkooleg7@gmail.com>
2025-11-23 14:25:57 +0200

    fix: made IEngineModule inherit from IModule
    - IEngineModule now extends IModule interface
    - This allows MapSystem, UIController, PlayerSystem, RenderingSystem to be used as IModule
    - Fixes type conversion errors in Engine::RegisterModule()
    
    All 4 game systems already inherit IEngineModule, so this fixes all remaining errors!

--- core/object/module/Interfaces/IEngineModule.h
+++ core/object/module/Interfaces/IEngineModule.h
@@ -1,34 +1,52 @@
 #ifndef IENGINEMODULE_H
-#define IENGINEMODULE_H
+#pragma once
 
+#include "IModule.h"
+#include "core/object/kernel/Core/Kernel.h"
 #include <string>
 #include <vector>
 
-class Kernel;
-
-class IEngineModule {
+// Interface for engine modules with kernel integration
+class IEngineModule : public IModule
+{
 public:
     virtual ~IEngineModule() = default;
-    
-    virtual const char* GetModuleName() const = 0;
-    virtual const char* GetModuleVersion() const = 0;
-    virtual const char* GetModuleDescription() const { return ""; }
-    
-    virtual bool Initialize(Kernel* kernel) = 0;
+
+    virtual const char *GetModuleName() const = 0;
+    virtual const char *GetModuleVersion() const = 0;
+    virtual const char *GetModuleDescription() const
+    {
+        return "";
+    }
+
+    virtual bool Initialize(Kernel *kernel) = 0;
     virtual void Shutdown() = 0;
-    virtual void Update(float deltaTime) {}
-    virtual void Render() {}
-    virtual void RegisterServices(Kernel* kernel) {}
-    virtual std::vector<std::string> GetDependencies() const { return {}; }
-    
-    bool IsInitialized() const { return m_initialized; }
-    void SetInitialized(bool value) { m_initialized = value; }
-    
+    virtual void Update(float deltaTime)
+    {
+    }
+    virtual void Render()
+    {
+    }
+    virtual void RegisterServices(Kernel *kernel)
+    {
+    }
+    virtual std::vector<std::string> GetDependencies() const
+    {
+        return {};
+    }
+
+    bool IsInitialized() const
+    {
+        return m_initialized;
+    }
+    void SetInitialized(bool value)
+    {
+        m_initialized = value;
+    }
+
 protected:
-    
 private:
     bool m_initialized = false;
 };
 
 #endif // IENGINEMODULE_H
-
