7824c0c5a712982539b4dea1ce197f35f60ea510
IOleg <guchkooleg7@gmail.com>
2025-11-23 13:53:29 +0200

    fix: restore JsonSerializableObject and create Engine.h
    - Restored all fields in JsonSerializableObject (type, color, etc.)
    - Created Engine.h to avoid forward declarations
    - Updated EngineApplication.h to include Engine.h directly
    
    No more forward declarations!

--- scene/resources/map/MapFileManager/Json/JsonMapFileManager.h
+++ scene/resources/map/MapFileManager/Json/JsonMapFileManager.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <vector>
 
+
 using json = nlohmann::json;
 
 // Serializable object for JSON
@@ -16,6 +17,20 @@ struct JsonSerializableObject
     Vector3 position;
     Vector3 rotation;
     float scale;
+    int type;
+
+    // Additional fields
+    bool visible = true;
+    std::string layer = "default";
+    std::string tags = "";
+    std::string color = "white";
+
+    // Sphere/shape properties
+    float radiusSphere = 0.0f;
+    float radiusH = 0.0f;
+    float radiusV = 0.0f;
+    Vector2 size = {0.0f, 0.0f};
+
     // Convert to/from JSON
     json ToJson() const;
     static JsonSerializableObject FromJson(const json &j);
