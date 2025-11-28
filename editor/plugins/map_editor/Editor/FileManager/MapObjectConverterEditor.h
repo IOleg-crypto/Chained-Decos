390b2c748222c770a8f1dcbe9418b75410827dbd
IOleg <guchkooleg7@gmail.com>
2025-11-28 16:40:19 +0200

    fixing same complie errors

--- editor/plugins/map_editor/Editor/FileManager/MapObjectConverterEditor.h
+++ editor/plugins/map_editor/Editor/FileManager/MapObjectConverterEditor.h
@@ -3,18 +3,18 @@
 
 #include "../Object/MapObject.h"
 #include "scene/resources/map/Converter/MapObjectConverter.h"
-#include "Engine/MapFileManager/Json/JsonMapFileManager.h"
-#include <ctime>
+#include "scene/resources/map/Core/MapData.h"
+#include "scene/resources/map/MapFileManager/Json/JsonMapFileManager.h"
 #include <cstdlib>
+#include <ctime>
 
 namespace MapObjectConverterEditor
 {
-    JsonSerializableObject MapObjectToJsonSerializableObject(const MapObject& obj);
-    MapObject JsonSerializableObjectToMapObject(const JsonSerializableObject& data);
-    
-    MapObjectData MapObjectToMapObjectData(const MapObject& obj);
-    MapObject MapObjectDataToMapObject(const MapObjectData& data);
-}
+JsonSerializableObject MapObjectToJsonSerializableObject(const MapObject &obj);
+MapObject JsonSerializableObjectToMapObject(const JsonSerializableObject &data);
+
+MapObjectData MapObjectToMapObjectData(const MapObject &obj);
+MapObject MapObjectDataToMapObject(const MapObjectData &data);
+} // namespace MapObjectConverterEditor
 
 #endif // MAPOBJECTCONVERTEREDITOR_H
-
