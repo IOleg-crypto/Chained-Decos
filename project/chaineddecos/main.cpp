c585a641632bcfbc8b78863940c74ff19d76711d
IOleg <124497826+IOleg-crypto@users.noreply.github.com>
2025-11-28 17:08:53 +0200

    update Chained Decos

--- project/chaineddecos/main.cpp
+++ project/chaineddecos/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     GameApplication gameApp(argc, argv);
     EngineApplication::Config engineConfig;
+    engineConfig.windowName = "Chained Decos";
     EngineApplication engine(engineConfig, &gameApp);
     engine.Run();
     return 0;
