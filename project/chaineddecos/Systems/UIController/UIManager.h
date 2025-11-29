0771c46715b68f319fccd440505691b6a62f788c
IOleg <guchkooleg7@gmail.com>
2025-11-29 15:03:21 +0200

    update engine architecture #4

--- project/chaineddecos/Systems/UIController/UIManager.h
+++ project/chaineddecos/Systems/UIController/UIManager.h
@@ -1,7 +1,7 @@
 #ifndef UI_CONTROLLER_H
 #define UI_CONTROLLER_H
 
-#include "core/engine/EngineApplication.h"
+#include "../../../core/engine/EngineApplication.h"
 #include "core/object/module/Interfaces/IEngineModule.h"
 #include "project/chaineddecos/Menu/Console/ConsoleManager.h"
 #include "project/chaineddecos/Menu/Menu.h"
@@ -14,8 +14,8 @@
 class UIManager : public IEngineModule
 {
 public:
-    UIController();
-    ~UIController() override;
+    UIManager();
+    ~UIManager() override;
 
     // IEngineModule interface
     const char *GetModuleName() const override
