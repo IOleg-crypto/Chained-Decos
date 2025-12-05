ffca7495bcfac2aa9656c3b963047d8eaa212cca
IOleg <guchkooleg7@gmail.com>
2025-12-05 18:37:38 +0200

    chore: clean up FetchContent artifacts from include/

--- include/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/gitnext/Chained Decos/include/raylib-src/src
-# Build directory: D:/gitnext/Chained Decos/include/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
