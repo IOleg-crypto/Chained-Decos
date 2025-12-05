443c1eb3acb35bda05749339985e7fc61692ba3b
IOleg <guchkooleg7@gmail.com>
2025-12-05 18:33:18 +0200

    refactor: replace FetchContent with git submodules for all dependencies

---
+++ include/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/gitnext/Chained Decos/include/googletest-src
+# Build directory: D:/gitnext/Chained Decos/include/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
