575e95888aa28c5acd241376afabeca254b4dd27
IOleg <guchkooleg7@gmail.com>
2026-01-03 17:44:31 +0200

    seperate game/engine editor

--- core/utils/core.h
+++
@@ -1,8 +0,0 @@
-﻿#ifndef CD_CORE_UTILS_CORE_H
-#define CD_CORE_UTILS_CORE_H
-
-// Core headers - simplified and flattened
-#include "core/application/application.h"
-#include "events/event.h"
-
-#endif // CD_CORE_UTILS_CORE_H
--- editor/logic/selection_manager.cpp
+++
@@ -1,7 +0,0 @@
-#include "selection_manager.h"
-
-namespace CHEngine
-{
-// Implementation moved to header for simplicity or removed if not needed.
-// This file is now empty but kept for compilation consistency if needed by CMake.
-} // namespace CHEngine
