7bc28a15b9943aa45bf20a07e7bb783db53b76f2
IOleg <guchkooleg7@gmail.com>
2026-01-03 17:51:25 +0200

    added render(refactor)

--- engine/application.cpp
+++ engine/application.cpp
@@ -1,4 +1,5 @@
 #include "application.h"
+#include "renderer.h"
 #include <raylib.h>
 
 namespace CH
@@ -7,14 +8,21 @@ Application *Application::s_Instance = nullptr;
 
 Application::Application(const Config &config)
 {
+    CH_CORE_ASSERT(!s_Instance, "Application already exists!");
     s_Instance = this;
+
     InitWindow(config.Width, config.Height, config.Title.c_str());
     SetTargetFPS(60);
     m_Running = true;
+
+    Renderer::Init();
+
+    CH_CORE_INFO("Application Initialized: %s (%dx%d)", config.Title, config.Width, config.Height);
 }
 
 Application::~Application()
 {
+    Renderer::Shutdown();
     CloseWindow();
     s_Instance = nullptr;
 }
@@ -26,19 +34,24 @@ void Application::Init(const Config &config)
 
 void Application::Shutdown()
 {
+    CH_CORE_INFO("Application Shutdown requested");
     s_Instance->m_Running = false;
 }
 
 void Application::PushLayer(Layer *layer)
 {
+    CH_CORE_ASSERT(layer, "Layer is null!");
     s_Instance->m_LayerStack.PushLayer(layer);
     layer->OnAttach();
+    CH_CORE_INFO("Layer Pushed: %s", layer->GetName());
 }
 
 void Application::PushOverlay(Layer *overlay)
 {
+    CH_CORE_ASSERT(overlay, "Overlay is null!");
     s_Instance->m_LayerStack.PushOverlay(overlay);
     overlay->OnAttach();
+    CH_CORE_INFO("Overlay Pushed: %s", overlay->GetName());
 }
 
 void Application::BeginFrame()
@@ -50,6 +63,9 @@ void Application::BeginFrame()
 
     BeginDrawing();
     ClearBackground(DARKGRAY);
+
+    for (Layer *layer : s_Instance->m_LayerStack)
+        layer->OnRender();
 
     for (Layer *layer : s_Instance->m_LayerStack)
         layer->OnImGuiRender();
@@ -67,6 +83,9 @@ bool Application::ShouldClose()
 
 void Application::OnEvent(Event &e)
 {
+    // Optional: too verbose?
+    // CH_CORE_TRACE("Event: %s", e.GetName());
+
     for (auto it = s_Instance->m_LayerStack.rbegin(); it != s_Instance->m_LayerStack.rend(); ++it)
     {
         if (e.Handled)
