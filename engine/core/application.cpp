e4aea0e82d7845e21b64599f080ea867c4cf2400
IOleg <guchkooleg7@gmail.com>
2026-01-31 15:57:45 +0200

    CI: Refactor workflows and CMake structure for professional artifact management

--- engine/core/application.cpp
+++ engine/core/application.cpp
@@ -22,8 +22,10 @@
 #include "rlgl.h"
 
 // GLFW
-#define GLFW_INCLUDE_NONE
-#include "external/glfw/include/glfw/glfw3.h"
+#ifndef GLFW_INCLUDE_NONE
+    #define GLFW_INCLUDE_NONE
+#endif
+#include <GLFW/glfw3.h>
 
 namespace CHEngine
 {
