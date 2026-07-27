dbface0c2355f02ef0588d0ea25742191a509ea7
I#Oleg <124497826+IOleg-crypto@users.noreply.github.com>
2026-07-27 13:46:45 +0300

    fix: suppress mpio GCC 14+ error and disable pack tests on CI
    - mpio/source/os.c passes char** to _spawvp() (expects const char* const*)
      GCC 14+ treats this as error. Add -Wno-error=incompatible-pointer-types.
    - Disable PACK_BUILD_TESTS to prevent TestPackPacker 'Not Run' ctest failure.

--- cmake/external/pack.cmake
+++ cmake/external/pack.cmake
@@ -2,7 +2,14 @@
 if(EXISTS "${CMAKE_SOURCE_DIR}/thirdparty/pack/CMakeLists.txt")
     set(gtest_force_shared_crt OFF CACHE BOOL "" FORCE)
     set(BUILD_SHARED_LIBS OFF)
+    set(PACK_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 
     add_subdirectory("${CMAKE_SOURCE_DIR}/thirdparty/pack"
         "${CMAKE_BINARY_DIR}/vendor/pack" EXCLUDE_FROM_ALL)
+
+    # GCC 14+ treats -Wincompatible-pointer-types as error.
+    # mpio/source/os.c passes char** to _spawvp() which expects const char* const*.
+    if(TARGET mpio-static)
+        target_compile_options(mpio-static PRIVATE -Wno-error=incompatible-pointer-types)
+    endif()
 endif()
